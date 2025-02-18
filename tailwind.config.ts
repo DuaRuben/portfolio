cd571c262740882a66c51fda552c7a87887d1427
Ruben <duaruben18@gmail.com>
2025-02-17 16:25:31 -0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,16 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		screens: {
+			  sm: '640px',
+			  md: '768px',
+			  lg: '1024px',
+			  xl: '1280px',
+		},
+		padding: {
+			  custom: '15px'
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],

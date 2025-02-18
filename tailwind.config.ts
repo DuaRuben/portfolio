1c6cb1bd846066bb1ff93e6e6b1d504d0bb40849
Ruben <duaruben18@gmail.com>
2025-02-17 17:02:46 -0800

    Ui Components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,32 +8,42 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-		container: {
-			center: true,
-			  padding: "15px",
-		},
-	  	screens: {
-		  sm: '640px',
-		  md: '768px',
-		  lg: '1024px',
-		  xl: '1280px',
-	  	},
-	  	fontFamily: {
-		  primary: 'var(--font-jetbrainsMono)',
-	  	},
-  		extend: {
-  			colors: {
-			  	primary: '#1c1c22',
-			  	accent:{
-				  	default:'#00ff99',
-				  	hover:'#00e187',
-			  	}
+  	container: {
+  		center: true,
+  		padding: '15px'
+  	},
+  	screens: {
+  		sm: '640px',
+  		md: '768px',
+  		lg: '1024px',
+  		xl: '1280px'
+  	},
+  	fontFamily: {
+  		primary: 'var(--font-jetbrainsMono)'
+  	},
+  	extend: {
+  		colors: {
+  			primary: '#1c1c22',
+  			accent: {
+  				default: '#00ff99',
+  				hover: '#00e187'
   			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		},
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],

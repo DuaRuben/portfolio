e99166dd9ad8e309278525d7a6bc771882d0286d
Ruben <duaruben18@gmail.com>
2025-02-17 16:54:06 -0800

    Customizing styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,25 +8,32 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
+		container: {
+			center: true,
+			  padding: "15px",
+		},
+	  	screens: {
+		  sm: '640px',
+		  md: '768px',
+		  lg: '1024px',
+		  xl: '1280px',
+	  	},
+	  	fontFamily: {
+		  primary: 'var(--font-jetbrainsMono)',
+	  	},
+  		extend: {
+  			colors: {
+			  	primary: '#1c1c22',
+			  	accent:{
+				  	default:'#00ff99',
+				  	hover:'#00e187',
+			  	}
+  			},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-		screens: {
-			  sm: '640px',
-			  md: '768px',
-			  lg: '1024px',
-			  xl: '1280px',
-		},
-		padding: {
-			  custom: '15px'
-		}
   	}
   },
   plugins: [require("tailwindcss-animate")],

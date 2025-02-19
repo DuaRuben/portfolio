ead3796874e3c0a3ff0dc6556008c75749a89b2b
Ruben <duaruben18@gmail.com>
2025-02-19 12:38:52 -0800

    Update layout.tsx
    To remove page transiton and stair transition components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.css";
 
 //components
 import Header from "@/components/Header";
-import PageTransition from "@/components/PageTransition";
-import StairTransition from "@/components/StairTransition";
-
 
 export const metadata: Metadata = {
   title: "Ruben's Portfolio",
@@ -26,10 +23,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${jetBrainsMono.variable} antialiased`} >
         <Header />
-        <StairTransition/>
-        <PageTransition>
+        <main>
             {children}
-        </PageTransition>
+        </main>
       </body>
     </html>
   );

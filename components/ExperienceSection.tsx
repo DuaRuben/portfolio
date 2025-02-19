cf19fb7ca51cb8b07280952a8e3d4521720a16f5
Ruben <duaruben18@gmail.com>
2025-02-19 11:48:35 -0800

    Create ExperienceSection.tsx

---
+++ components/ExperienceSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from "@/components/Section";
+
+const ExperienceSection = () => {
+    return (
+        <Section id="experience">
+            <h1>Experience Section</h1>
+        </Section>
+    );
+};
+
+export default ExperienceSection;

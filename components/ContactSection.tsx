504cce1c7904384987ec67573a8245b950af5ae1
Ruben <duaruben18@gmail.com>
2025-02-19 11:48:23 -0800

    Create ContactSection.tsx

---
+++ components/ContactSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from "@/components/Section";
+
+const ContactSection = () => {
+    return (
+        <Section id="contact">
+            <h1>Contact Section</h1>
+        </Section>
+    );
+};
+
+export default ContactSection;

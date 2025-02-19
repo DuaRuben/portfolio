221ba3ff342535e567bb11fef659f4430cc0205d
Ruben <duaruben18@gmail.com>
2025-02-19 11:49:42 -0800

    Create SkillSection.tsx

---
+++ components/SkillSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from "@/components/Section";
+
+const SkillSection = () => {
+    return (
+        <Section id="skills">
+            <h1>Skills Section</h1>
+        </Section>
+    );
+};
+
+export default SkillSection;

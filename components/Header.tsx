667a146f7103511eac39953db9333ec32a371a85
Ruben <duaruben18@gmail.com>
2025-02-19 11:48:40 -0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Nav from "@/components/Nav";
 import MobileNav from "@/components/MobileNav";
 
 const Header = () => {
     return (
-        <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto flex items-center justify-between ">
+        <header className="fixed top-0 left-0 w-full bg-primary bg-opacity-90 backdrop-blur-lg z-50 py-4 xl:py-6 text-white shadow-md">
+            <div className="container mx-auto flex items-center justify-between px-6">
                 <Link href="/">
                     <h1 className="text-4xl font-semibold">
-                        Ruben <span className="text-accent-default">Dua</span></h1>
+                        Ruben <span className="text-accent-default">Dua</span>
+                    </h1>
                 </Link>
 
-                {/*Style for Desktop navigation menu*/}
+                {/* Desktop navigation menu */}
                 <div className="hidden xl:flex items-center gap-8">
                     <Nav />
                     <a href="/Ruben_Resume_Full_Stack.pdf" download>
@@ -21,8 +22,10 @@ const Header = () => {
                     </a>
                 </div>
 
-                {/*Style for Mobile navigation menu*/}
-                <div className="xl:hidden"> <MobileNav/> </div>
+                {/* Mobile navigation menu */}
+                <div className="xl:hidden">
+                    <MobileNav />
+                </div>
             </div>
         </header>
     );

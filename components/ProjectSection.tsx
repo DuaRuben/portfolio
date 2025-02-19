e5f9d5ddf1202e9e3ca3cb16638b58a00f5993ae
Ruben <duaruben18@gmail.com>
2025-02-19 11:49:32 -0800

    Create ProjectSection.tsx

---
+++ components/ProjectSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from "@/components/Section";
+
+const ProjectSection = () => {
+    return (
+        <Section id="projects">
+            <h1>Projects Section</h1>
+        </Section>
+    );
+};
+
+export default ProjectSection;

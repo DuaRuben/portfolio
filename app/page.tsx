53a4cf15b89769e4182d17f5e554ca3e2e54e721
Ruben <duaruben18@gmail.com>
2025-02-19 11:48:14 -0800

    Update page.tsx
    Created differenct section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
 
+import Section from "@/components/Section";
+import HomeSection from "@/components/HomeSection";
+import SkillSection from "@/components/SkillSection";
+import EducationSection from "@/components/EducationSection";
+import ProjectSection from "@/components/ProjectSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import ContactSection from "@/components/ContactSection";
+
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+      <main className="pt-20 xl:pt-24">
+          <HomeSection/>
+          <SkillSection/>
+          <ProjectSection/>
+          <ExperienceSection/>
+          <EducationSection/>
+          <ContactSection/>
+      </main>
+  );
 }

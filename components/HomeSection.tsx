2cc6a626fee001d52d353f9f721bf1c9fcbde2dd
Ruben <duaruben18@gmail.com>
2025-02-19 11:48:56 -0800

    Create HomeSection.tsx

---
+++ components/HomeSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from "@/components/Section";
+
+const HomeSection = () => {
+    return (
+        <Section id="home">
+            <h1>Home Section</h1>
+        </Section>
+    );
+};
+
+export default HomeSection;

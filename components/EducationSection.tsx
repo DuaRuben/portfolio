e66381c2476cece05fb849733e738e2373c103ff
Ruben <duaruben18@gmail.com>
2025-02-19 11:48:28 -0800

    Create EducationSection.tsx

---
+++ components/EducationSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from "@/components/Section";
+
+const EducationSection = () => {
+    return (
+        <Section id="education">
+            <h1>Education Section</h1>
+        </Section>
+    );
+};
+
+export default EducationSection;

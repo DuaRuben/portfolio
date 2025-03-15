8ff70a61b0f65c4079f038b6047a3a77ebf0555f
Ruben <duaruben18@gmail.com>
2025-03-15 16:00:33 -0700

    Update Section.tsx

--- components/Section.tsx
+++ components/Section.tsx
@@ -3,11 +3,12 @@ import React from "react";
 interface SectionProps {
     id: string,
     children: React.ReactNode,
+    className?: string
 }
 
-const Section: React.FC<SectionProps> = ({id, children}) => {
+const Section: React.FC<SectionProps> = ({id, children, className}) => {
     return (
-        <section id={id} className="min-h-screen flex items-center justify-center p-8">
+        <section id={id} className={'min-h-screen flex items-center justify-center p-8 ${className}'}>
             {children}
         </section>
     );
